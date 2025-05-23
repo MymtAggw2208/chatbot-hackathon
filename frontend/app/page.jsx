62a5b1759cff5c0d6e71f99bbb96b246bb88d756
m(ura)sak <misaki.murasakix1@gmail.com>
2025-05-23 18:07:21 +0900

    タイトルの変更、ボタンの微調整

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-2 md:p-4">
       <div className="z-10 w-full max-w-5xl flex flex-col h-full">
         <h1 className="text-xl md:text-3xl font-bold text-center my-4 text-orange-600">
-          学びの友だち - AIと一緒に考えよう！
+          学びのお友達 ラーニーちゃんと一緒に考えよう！
         </h1>
         <LearningCompanion />
       </div>
